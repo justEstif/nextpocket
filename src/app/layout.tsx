a4081aa88bb6e8ce5ad7359feb680241deb8f953
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-11-07 07:05:28 -0500

    Add picocss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import "@picocss/pico";
 
 export default function RootLayout({
   children,
@@ -8,15 +9,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>
-          <nav>
-            <Link href="/">Home</Link>
-            <Link href="/notes">Notes</Link>
-          </nav>
+        <header className="container">
+          <NavBar />
         </header>
-
-        <main>{children}</main>
+        <main className="container">{children}</main>
       </body>
     </html>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link href="/notes">Notes</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
